ca0fdb0e2bef2e3cfb0c810e563e79894d0d99cb
Filpos_ <fifokrapek@gmail.com>
2026-04-09 19:02:06 +0200

    changes to the code

--- sketch_nov6a/sketch_nov6a.ino
+++ sketch_nov6a/sketch_nov6a.ino
@@ -1,21 +1,22 @@
-// 27cm/s -rchlosť robota
+// 27cm/s rýchlosť robota
 // zistiť dialku aku dojazdi po vypnuti motora (cm)
-// musime spraviť abz sa otáčal okolo stredu krzžovatky
+// musime spraviť aby sa otáčal okolo stredu kryžovatky
 int tas=0;
- 
-float block = 15; //cm
+
+float block = 30; //cm
 int ir = 650; //0 - 1000
 bool button = false;
+int delAfterKrizovatkaTurn = 500;
 
 void setup() {
-  
+
  pinMode(LED_BUILTIN, OUTPUT);
  pinMode(30, INPUT); //button
 
  pinMode(5,OUTPUT);
  pinMode(6,OUTPUT);
  pinMode(7,OUTPUT);
- 
+
  pinMode(2,OUTPUT);
  pinMode(3,OUTPUT);
  pinMode(4,OUTPUT);
@@ -28,15 +29,15 @@ void setup() {
 
  pinMode(8, OUTPUT);
  pinMode(9, INPUT);
+
  Serial.begin(9600);
- 
- 
+
 }
 
 void loop() {
-   
+
    tas=0;
-   
+
    if(analogRead(A0) < ir ){tas++;}
    if(analogRead(A1) < ir ){tas++;}
    if(analogRead(A2) < ir ){tas++;}
@@ -45,25 +46,32 @@ void loop() {
 
    if(digitalRead(30) == true){
       button = !button;
+      delay(250);
    }
-   
+
    if(button){
       if(tas >= 3){
-         stop() //vpred(0); // ked dojazdi + vpred(int del) tak aby stred robota bol v strede krizovatky
+         vpred(296); // vzdialenosť senzora od stredu motora (8cm) / rychlost robota
+         //vpred(0); // ked dojazdi + vpred(int del) tak aby stred robota bol v strede krizovatky
          if(ultraZvuk(3)<block){
             turn(true, 250);
-            
+            stop(); delay(delAfterKrizovatkaTurn);
+
             if(ultraZvuk(3)<block){
                turn(false, 250);
+               stop(); delay(delAfterKrizovatkaTurn);
                turn(false, 250);
-               
+               stop(); delay(delAfterKrizovatkaTurn);
+
                if(ultraZvuk(3)<block){
                   turn(false, 250);
-               }
-            }
-         }
-         
-         
+                  vpred(-1);
+                  stop(); delay(delAfterKrizovatkaTurn);
+               }else{vpred(-1);}
+            }else{vpred(-1);}
+         }else{vpred(-1);}
+
+
       }else if(analogRead(A0) < ir){
          turn(true, 0);
       }else if(analogRead(A4) < ir){
@@ -72,32 +80,32 @@ void loop() {
          vpred(-1);
          }
    }else{
-      stop()
+      stop();
    }
   //if(digitalRead(30)){vpred(3000);}
-  
+
 
 }
 
 int vpred(int del){
-   
+
    digitalWrite(5, LOW);  // dozadu
    digitalWrite(6, HIGH);  // dopredu
    analogWrite(7, 250);   // rzchlosť
- 
+
    analogWrite(2, 1);  // rzchlosť
    digitalWrite(3, HIGH);  // dopredu
    digitalWrite(4, LOW);   // dozadu
-   
+
    if(del == -1){
       return 0;
    }
    delay(del);
-   
+
    digitalWrite(5, LOW);  // dozadu
    digitalWrite(6, LOW);  // dopredu
    analogWrite(7, 0);   // rzchlosť
- 
+
    analogWrite(2, 0);  // rzchlosť
    digitalWrite(3, LOW);  // dopredu
    digitalWrite(4, LOW);   // dozadu
@@ -108,7 +116,7 @@ void stop(){
    digitalWrite(5, LOW);  // dozadu
    digitalWrite(6, LOW);  // dopredu
    analogWrite(7, 0);   // rzchlosť
- 
+
    analogWrite(2, 0);  // rzchlosť
    digitalWrite(3, LOW);  // dopredu
    digitalWrite(4, LOW);   // dozadu
@@ -119,7 +127,7 @@ int turn(bool left, int del){
       digitalWrite(5, LOW);  // dozadu
       digitalWrite(6, HIGH);  // dopredu
       analogWrite(7, 5);   // rzchlosť
- 
+
       analogWrite(2, 0);  // rzchlosť
       digitalWrite(3, LOW);  // dopredu
       digitalWrite(4, HIGH);   // dozadu
@@ -127,21 +135,21 @@ int turn(bool left, int del){
       digitalWrite(5, HIGH);  // dozadu
       digitalWrite(6, LOW);  // dopredu
       analogWrite(7, 5);   // rzchlosť
- 
+
       analogWrite(2, 0);  // rzchlosť
       digitalWrite(3, HIGH);  // dopredu
       digitalWrite(4, LOW);   // dozadu
    }
-   
+
    if(del == -1){
       return 0;
    }
    delay(del);
-   
+
    while(true){
       if(analogRead(A2) < 650){break;}
    }
-   
+
    return 1;
 }
 
@@ -169,7 +177,7 @@ float ultraZvuk(int repeat){
 //   digitalWrite(5, HIGH);  // dozadu
 //   digitalWrite(6, LOW);  // dopredu
 //   analogWrite(7, 5);   // rzchlosť
-// 
+//
 //   analogWrite(2, 0);  // rzchlosť
 //   digitalWrite(3, HIGH);  // dopredu
 //   digitalWrite(4, LOW);   // dozadu
